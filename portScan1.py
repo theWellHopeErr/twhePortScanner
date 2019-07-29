b7cb150fae33c9db409dbdc737a58bd9f5da2de7
theWellHopeErr <ssuryarajan@gmail.com>
2019-07-29 10:21:43 +0530

    Add Commit

--- portScan1.py
+++ portScan1.py
@@ -2,12 +2,13 @@
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#AF_INET is used to as (host,port) format is used
 
-host = "192.168.1.4"
-port = 444
+host = "172.16.1.185"    #IP Address of user's PC
+port = 444		#Port that needs to be checked
+
 def portScan(port):
-	if sock.connect_ex((host,port)):
+	if sock.connect_ex((host,port)): #connect_ex return 0 if connection has succeeded else returns errno
 		print("Port %d is closed" % (port))
 	else:
 		print("Port %d is open" % (port))
