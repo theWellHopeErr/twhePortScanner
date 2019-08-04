328f115fcb7735ba5222f666607da4e4f5ce5ea4
theWellHopeErr <ssuryarajan@gmail.com>
2019-08-04 09:59:47 +0530

    Add returnBanner.py

---
+++ returnBanner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python  
+import socket  
+def grab_banner(ip_address,port):  
+     try:  
+          s=socket.socket()  
+          s.connect((ip_address,port))  
+          return s.recv(1024)  
+     except:  
+          return  
+          
+def main():
+    portList = [21,22,23]  
+    ip_address = '192.168.1.1'
+    for port in portList:  
+        banner = grab_banner(ip_address,port)  
+        print ip_address + '/' + str(port) + ':' + banner  
+        
+if __name__ == '__main__':  
+      main() 
